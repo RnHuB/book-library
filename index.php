5a8ac081770f4370d77cb8817db5817f532f56f1
rnhub <dayal.rn@gmail.com>
2021-08-04 17:57:39 +0530

    my first commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <title>Library</title>
+   
+</head>
+<body>
+         <header>
+                <div id='logo'>
+                 <img src="bookLogo.png" alt="error">
+                </div>
+                <h1>Library</h1>
+            <div id='search'>
+                <input type="text" placeholder="Search">
+                <button>djs</button>
+            </div>
+            <div id='loginBar'>
+                <span><a href='./login.html'>User Login</a></span>
+                <span>|</span>
+                <span><a href='#'>User Signup</a></span>
+                <span>|</span>
+                <span><a href='#'>Admin Login</a></span>
+            </div>
+        </header>
+        <nav>
+            <ul>
+                <li><a href='#'>Home</a></li>
+                <li><a href='#'>Archive</a></li>
+                <li><a href='#'>History</a></li>
+                <li><a href='#'>Gallery</a></li>
+                <li><a href='#'>About Us</a></li>
+            </ul>
+        </nav>
+        <main>
+            <div id='sideBar'>
+
+            </div>
+        </main>
+        <footer>
+            
+        </footer>
+</body>
+</html>
